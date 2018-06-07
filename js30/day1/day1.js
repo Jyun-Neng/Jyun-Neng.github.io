0489585f18573c2209a843ed4ce53a032fd919de
Jyun-NengJi <jyunnengji@gmail.com>
2018-06-08 01:05:22 +0800

    click event

--- js30/day1/day1.js
+++ js30/day1/day1.js
@@ -1,6 +1,17 @@
 window.addEventListener('keydown', playSounds);
+window.addEventListener('click', playSoundsByClick);
+
+function playSoundsByClick (e) {
+    const key_id = e.target.id;
+    const audio = document.querySelector(`audio[key-data="${key_id}"]`);
+
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+}
 
 function playSounds(e) {
+    console.log(e.key);
     const audio = document.querySelector(`audio[key-data="${e.keyCode}"]`);
     const key = document.querySelector(`.key[key-data="${e.keyCode}"]`);
     
